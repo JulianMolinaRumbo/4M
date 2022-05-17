4d67e7203bbc6f2579cc208e7b95e2100f3f16ed
pedronunezcode <pedronunezcode@gmail.com>
2022-05-16 20:22:02 -0700

    push for office

--- 4M/js/home.js
+++ 4M/js/home.js
@@ -15,40 +15,40 @@ $(document).ready(function () {
 	})
 });
 
-window.onload = function () {
-	var images = ['./Images/FONDOS/Background/1.png', './Images/FONDOS/Background/2.png', './Images/FONDOS/Background/3.jpg'];
-	var image = images[Math.floor(Math.random() * images.length)];
-	if (image === './Images/FONDOS/Background/1.png') {
-		document.getElementById('section-1').style.backgroundImage = "url('" + image + "')";
-		// document.getElementsByTagName('body')[0].style.;
-		var element = document.getElementById('guerrero-toggle');
-		element.classList.add("show-image");
-		document.addEventListener("mousemove", parallax);
-		function parallax(e) {
-			this.querySelectorAll('#guerrero-toggle').forEach(guerrerotoggle => {
-				const speed = guerrerotoggle.getAttribute('data-speed')
-				const x = (window.innerWidth - e.pageX * speed) / 100
-				const y = (window.innerWidth - e.pageY * speed) / 100
+// window.onload = function () {
+// 	var images = ['./Images/FONDOS/Background/1.png', './Images/FONDOS/Background/2.png', './Images/FONDOS/Background/3.jpg'];
+// 	var image = images[Math.floor(Math.random() * images.length)];
+// 	if (image === './Images/FONDOS/Background/1.png') {
+// 		document.getElementById('section-1').style.backgroundImage = "url('" + image + "')";
+// 		// document.getElementsByTagName('body')[0].style.;
+// 		var element = document.getElementById('guerrero-toggle');
+// 		element.classList.add("show-image");
+// 		document.addEventListener("mousemove", parallax);
+// 		function parallax(e) {
+// 			this.querySelectorAll('#guerrero-toggle').forEach(guerrerotoggle => {
+// 				const speed = guerrerotoggle.getAttribute('data-speed')
+// 				const x = (window.innerWidth - e.pageX * speed) / 100
+// 				const y = (window.innerWidth - e.pageY * speed) / 100
 
-				guerrerotoggle.style.transform = `translateX(${x}px) translateY(${y}px)`
-			});
-		}
+// 				guerrerotoggle.style.transform = `translateX(${x}px) translateY(${y}px)`
+// 			});
+// 		}
 
-	} else {
-		document.getElementById('section-1').style.backgroundImage = "url('" + image + "')";
-		// document.getElementsByTagName('body')[0].style.backgroundImage = "url('" + image + "')";
-		document.addEventListener("mousemove", parallax);
-		function parallax(e) {
-			this.querySelectorAll('.M').forEach(M => {
-				const speed = M.getAttribute('data-speed')
-				const x = (window.innerWidth - e.pageX * speed) / 100
-				const y = (window.innerWidth - e.pageY * speed) / 100
+// 	} else {
+// 		document.getElementById('section-1').style.backgroundImage = "url('" + image + "')";
+// 		// document.getElementsByTagName('body')[0].style.backgroundImage = "url('" + image + "')";
+// 		document.addEventListener("mousemove", parallax);
+// 		function parallax(e) {
+// 			this.querySelectorAll('.M').forEach(M => {
+// 				const speed = M.getAttribute('data-speed')
+// 				const x = (window.innerWidth - e.pageX * speed) / 100
+// 				const y = (window.innerWidth - e.pageY * speed) / 100
 
-				M.style.transform = `translateX(${x}px) translateY(${y}px)`
-			});
-		}
-	}
-}
+// 				M.style.transform = `translateX(${x}px) translateY(${y}px)`
+// 			});
+// 		}
+// 	}
+// }
 
 function openNav() {
 	document.getElementById("mySidenav").style.width = "80%";
